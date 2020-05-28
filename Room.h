f7cdc0524b7fe2a84dda2249304b6b6e71f43e16
Xofrats <42830990+Xofrats@users.noreply.github.com>
2020-05-28 23:03:02 +0200

    Update Room.h
    added humidity sensor

--- Room.h
+++ Room.h
@@ -1,11 +1,13 @@
 #pragma once
-#include "FreeRTOS.h"
-#include "task.h"
+#include <ATMEGA_FreeRTOS.h>
+#include <task.h>
 #include "co2.h"
+#include "humidity.h"
 
 typedef struct Room* pRoom;
 
 void* co2_sensor;
+void* humidity_sensor;
 
 pRoom room_create();
 void room_destroy(pRoom self);
