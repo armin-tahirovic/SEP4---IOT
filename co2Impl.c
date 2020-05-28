e23ebfa507ce8220c866d048c669d73213ddb9e0
Xofrats <42830990+Xofrats@users.noreply.github.com>
2020-05-28 23:05:45 +0200

    Update co2Impl.c
    Added driver to mh_z19 sensor

--- co2Impl.c
+++ co2Impl.c
@@ -1,38 +1,51 @@
 #include "co2.h"
+#include <mh_z19.h>
 
 typedef struct CO2 {
-  int driverPort;
-  int co2;
+	uint16_t driverPort;
+	uint16_t co2;
 }CO2_t;
 
-int co2_create() {
-  pCO2 driverPort = (pCO2)pvPortMalloc(sizeof(CO2_t));
+uint16_t co2_create() {
+	pCO2 driverPort = (pCO2)pvPortMalloc(sizeof(CO2_t));
 
-  if (NULL == driverPort)
-    return NULL;
+	if (NULL == driverPort)
+	return NULL;
 
-  return driverPort;
+	return driverPort;
 
 }
 
 void co2_destroy(pCO2 self) {
-  vPortFree(self);
+	vPortFree(self);
 }
 
-void meassure(pCO2 self) {
-  self->co2 = rand() % 50;
+
+
+void co2_meassure(pCO2 self) {
+
+			mh_z19_take_meassuring();
+			PORTA ^= _BV(PA7);
+	
 }
 
-int getCO2(pCO2 self) {
+uint16_t getCO2(pCO2 self) {
 
-  return self->co2;
+	return self->co2;
+}
+
+void setCO2(pCO2 self, uint16_t ppm) {
+
+	self->co2 = ppm;
 }
 
 void task_CO2(void* pvParameters) {
-  (void)pvParameters;
+	(void)pvParameters;
 
-  for (;;) {
-    vTaskDelay(pdMS_TO_TICKS(200));
-    meassure(co2_sensor);
-  }
+
+	for (;;) {
+	
+		vTaskDelay(pdMS_TO_TICKS(1500));
+		co2_meassure(co2_sensor);
+	}
 }
