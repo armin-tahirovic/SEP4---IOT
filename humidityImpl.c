52de7d1c7b8432c31739cdcf9789505456298d6c
Thommelise <42830986+Thommelise@users.noreply.github.com>
2020-06-01 09:56:54 +0200

    Update humidityImpl.c

--- humidityImpl.c
+++ humidityImpl.c
@@ -1,5 +1,5 @@
 #include "humidity.h"
-
+#include "CO2.h"
 typedef struct humidity {
 	int driverPort;
 	uint16_t humidity;
@@ -26,13 +26,15 @@ void humidity_meassure(pHumidity self) {
 
 
 uint16_t getHumidity(pHumidity self) {
-
+	xSemaphoreTake(xSemaphore, portMAX_DELAY);
 	return self->humidity;
+	xSemaphoreGive(xSemaphore);
 }
 
 void setHumidity(pHumidity self, uint16_t newMeasure) {
-
+	xSemaphoreTake(xSemaphore, portMAX_DELAY);
 	self->humidity = newMeasure;
+	xSemaphoreGive(xSemaphore);
 }
 
 
@@ -42,11 +44,12 @@ void task_Humidity(void* pvParameters) {
 
 	for (;;) {
 		hih8120Wakeup();
+		xEventGroupSetBits(xEventGroup, BIT_4);
 		vTaskDelay(pdMS_TO_TICKS(1000));
 		humidity_meassure(pvParameters);
-		 uint16_t newHumidity = hih8120GetHumidityPercent_x10();
-		 int16_t newTemperature = hih8120GetTemperature_x10();
-		 
+		uint16_t newHumidity = hih8120GetHumidityPercent_x10();
+		int16_t newTemperature = hih8120GetTemperature_x10();
+
 		printf("From Humidity Class");
 		printf("Humidity: %d and temperature: %d \n", newHumidity, newTemperature);
 
