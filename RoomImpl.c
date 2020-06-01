4526d8d2ff407097973830148f8a1f6b93b7e35e
Thommelise <42830986+Thommelise@users.noreply.github.com>
2020-06-01 10:07:07 +0200

    Update RoomImpl.c

--- RoomImpl.c
+++ RoomImpl.c
@@ -23,9 +23,14 @@ void taskRoomCollect(void* pvParameters) {
   (void)pvParameters;
 
   for (;;) {
+    EventBits_t uxBits = xEventGroupWaitBits(xEventGroup, BIT_0 | BIT_1 | BIT_2 | BIT_3 | BIT_4, pdTRUE, pdTRUE, 500);
+    if((uxBits & (BIT_0 | BIT_1 | BIT_2 | BIT_3 | BIT_4)) == (BIT_0 | BIT_1 | BIT_2 | BIT_3 | BIT_4)){
     vTaskDelay(pdMS_TO_TICKS(500));
     int co2_meassure = getCO2(co2_sensor);
 
     printf("CO2: %i\n", co2_meassure);
+    
+    xEventGroupClearBits(xEventGroup, BIT_0 | BIT_1 | BIT_2 | BIT_3 | BIT_4);
+    }
   }
 }
