230cf7fe96036dd007aa101c00cddf3a790dc3b9
armin-tahirovic <43197986+armin-tahirovic@users.noreply.github.com>
2020-06-03 11:01:21 +0200

    Update Room.h

--- Room.h
+++ Room.h
@@ -4,12 +4,11 @@
 #include "co2.h"
 #include "humidity.h"
 #include "CO2.h"
+#include "loraWanDriver.h"
 
 typedef struct Room* pRoom;
 
 void* co2_sensor;
 void* humidity_sensor;
 
-pRoom room_create();
-void room_destroy(pRoom self);
 void taskRoomCollect(void* pvParameters);
