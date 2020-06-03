af2deaa10481495495b8ccaed9f52a7440c9ba55
armin-tahirovic <43197986+armin-tahirovic@users.noreply.github.com>
2020-06-03 10:52:47 +0200

    Add files via upload

---
+++ loraWanDriver.h
@@ -0,0 +1,13 @@
+#include "Room.h"
+
+QueueHandle_t xQueue;
+
+typedef struct LoraDriver* pLora;
+typedef struct LoraDriver {
+  uint8_t len;
+  uint8_t* bytes;
+} LoraDriver_t;
+
+void queueCreate();
+void queue_lora(pLora mes);
+void taskLoraDriver(void* pvParameters);
---
+++ loraWanPaylod.c
@@ -0,0 +1,42 @@
+#include "loraWanDriver.h"
+
+void queueCreate() {
+  xQueue = xQueueCreate(10, sizeof(pLora));
+}
+
+void queue_lora(pLora mes) {
+  xQueueSend(
+    xQueue,
+    &mes,
+    0u);
+}
+
+void taskLoraDriver(void* pvParameters) {
+  queueCreate();
+  (void)pvParameters;
+  pLora message = pvParameters;
+
+  for (;;)
+  {
+    BaseType_t em = xQueueReceive(
+      xQueue,
+      &message,
+      5000);
+
+    em = pdTRUE;
+
+    /*xSemaphoreTake(xSemaphoreMutex, portMAX_DELAY);
+    printf("Length: %i \n", message->len);
+    xSemaphoreGive(xSemaphoreMutex);
+
+    xSemaphoreTake(xSemaphoreMutex, portMAX_DELAY);
+    for (size_t i = 0; i < message->len; i++)
+    {
+      printf("Bytes: %x \n", message->bytes[i]);
+    }
+    xSemaphoreGive(xSemaphoreMutex);*/
+
+    vPortFree(message->bytes);
+    vPortFree(message);
+  }
+}
