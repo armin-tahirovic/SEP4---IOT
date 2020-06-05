9052014b9da6402d4e00dcac2485a13ac26bad43
Xofrats <42830990+Xofrats@users.noreply.github.com>
2020-06-05 10:18:41 +0200

    co2 uden semaphore

--- co2Impl.c
+++ co2Impl.c
@@ -1,16 +1,14 @@
 #include "co2.h"
-#include <mh_z19.h>
 
 typedef struct CO2 {
-	uint16_t driverPort;
 	uint16_t co2;
 }CO2_t;
 
-uint16_t co2_create() {
+pCO2 co2_create() {
 	pCO2 driverPort = (pCO2)pvPortMalloc(sizeof(CO2_t));
 
 	if (NULL == driverPort)
-		return NULL;
+	return NULL;
 
 	return driverPort;
 
@@ -20,35 +18,37 @@ void co2_destroy(pCO2 self) {
 	vPortFree(self);
 }
 
-
+mh_z19_return_code_t rc;
 
 void co2_meassure(pCO2 self) {
-
-	mh_z19_take_meassuring();
-	PORTA ^= _BV(PA7);
-
+	
+	rc = mh_z19_take_meassuring();
+	if (rc != MHZ19_OK)
+	{
+		printf("something wrong: \n");
+	}
+			printf("new co2: \n");
+	
+	
 }
 
 uint16_t getCO2(pCO2 self) {
-	xSemaphoreTake(xSemaphore, portMAX_DELAY);
 	return self->co2;
-	xSemaphoreGive(xSemaphore);
 }
 
 void setCO2(pCO2 self, uint16_t ppm) {
-	xSemaphoreTake(xSemaphore, portMAX_DELAY);
+	
 	self->co2 = ppm;
-	xSemaphoreGive(xSemaphore);
+
 }
+
 
 void task_CO2(void* pvParameters) {
 	(void)pvParameters;
-
-
 	for (;;) {
-		xEventGroupSetBits(xEventGroup, BIT_0);
-		vTaskDelay(pdMS_TO_TICKS(1500));
-		co2_meassure(co2_sensor);
+	
+		vTaskDelay(pdMS_TO_TICKS(5000));
+		co2_meassure(pvParameters);
+			
 	}
 }
-
