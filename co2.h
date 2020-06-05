7c5b1bbb97e0ae68599191ae4a5a9b295c944f19
Xofrats <42830990+Xofrats@users.noreply.github.com>
2020-06-05 10:17:38 +0200

    Update co2.h

--- co2.h
+++ co2.h
@@ -1,21 +1,25 @@
 #pragma once
-#include "Room.h"
-#include "event_groups.h"
-#include "semphr.h"
+
+#include <ATMEGA_FreeRTOS.h>
+#include <mh_z19.h>
+#include <stdio.h>
+#include <task.h>
+
+uint16_t new_co2_meassure;
+
 
 typedef struct CO2* pCO2;
 
-uint16_t co2_create();
+pCO2 co2_create();
 void co2_destroy(pCO2 self);
-void co2_meassure(pCO2 self);
+void co2_meassure();
 uint16_t getCO2(pCO2 self);
 void setCO2(pCO2 self, uint16_t ppm);
+
 void task_CO2(void* pvParameters);
 
-#define BIT_0 ( 1 << 0)
-#define BIT_1 ( 1 << 2)
-#define BIT_2 (1 << 3)
-#define BIT_3 (1 << 4)
-#define BIT_4 (1 << 5)
+
+
+
 EventGroupHandle_t xEventGroup;
 SemaphoreHandle_t xSemaphore;
