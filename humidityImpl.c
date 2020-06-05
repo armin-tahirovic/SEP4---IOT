35566c45f168ee78182b62b137e58cc57ee97153
Xofrats <42830990+Xofrats@users.noreply.github.com>
2020-06-05 10:22:33 +0200

    humidity og temperature sammen

--- humidityImpl.c
+++ humidityImpl.c
@@ -1,14 +1,13 @@
 #include "humidity.h"
 
 typedef struct humidity {
-	int driverPort;
 	uint16_t humidity;
 	int16_t temperature;
 }humidity_t;
 
 
 
-int humidity_create() {
+pHumidity humidity_create() {
 	pHumidity driverPort = (pHumidity)pvPortMalloc(sizeof(humidity_t));
 
 	if (NULL == driverPort) {
@@ -19,39 +18,47 @@ int humidity_create() {
 
 }
 
-void humidity_meassure(pHumidity self) {
-	hih8120Meassure();
+void humidity_destroy(pHumidity self){
+	vPortFree(self);
 }
 
+void humidity_meassure(pHumidity self) {
+	hih8120Meassure();
+	setHumidity(self, hih8120GetHumidityPercent_x10());
+	setTemperature(self, hih8120GetTemperature_x10());
+	printf("Humidity and temperature: %d and %d \n", self->humidity, self->temperature);
+}
 
+int16_t getTemperature(pHumidity self){
+	return self->temperature;
+}
+
+void setTemperature(pHumidity self, int16_t newTemperature){
+	self->temperature = newTemperature;
+}
 
 uint16_t getHumidity(pHumidity self) {
-	xSemaphoreTake(xSemaphore, portMAX_DELAY);
+
 	return self->humidity;
-	xSemaphoreGive(xSemaphore);
 }
 
 void setHumidity(pHumidity self, uint16_t newMeasure) {
-	xSemaphoreTake(xSemaphore, portMAX_DELAY);
+
 	self->humidity = newMeasure;
-	xSemaphoreGive(xSemaphore);
 }
 
 
 void task_Humidity(void* pvParameters) {
-	(void)pvParameters;
-	pHumidity data = pvParameters;
 
+	hih8120Wakeup();
+	humidity_meassure(pvParameters);
+	
+	printf("Humidity task started \n");
 	for (;;) {
-		hih8120Wakeup();
-		xEventGroupSetBits(xEventGroup, BIT_4);
-		vTaskDelay(pdMS_TO_TICKS(1000));
+		
+		vTaskDelay(pdMS_TO_TICKS(59000));
 		humidity_meassure(pvParameters);
-		uint16_t newHumidity = hih8120GetHumidityPercent_x10();
-		int16_t newTemperature = hih8120GetTemperature_x10();
-
-		printf("From Humidity Class");
-		printf("Humidity: %d and temperature: %d \n", newHumidity, newTemperature);
+		
 
 	}
 }
