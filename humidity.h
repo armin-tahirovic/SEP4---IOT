eeebb322cd7cdea586992aec764a0d2c73621892
Xofrats <42830990+Xofrats@users.noreply.github.com>
2020-06-05 10:21:38 +0200

    setter og getter

--- humidity.h
+++ humidity.h
@@ -1,17 +1,21 @@
 #pragma once
 #include <ATMEGA_FreeRTOS.h>
+#include <hih8120.h>
 #include <stdint.h>
-#include "task.h"
-#include "co2.h"
+#include <stdio.h>
+#include <task.h>
 
 typedef struct humidity* pHumidity;
 
-int humidity_create();
+pHumidity humidity_create();
+void humidity_destroy(pHumidity self);
 
 void humidity_meassure(pHumidity self);
 
-uint16_t getHumidity(pHumidity self);
+int16_t getTemperature(pHumidity self);
+void setTemperature(pHumidity self, int16_t newTemperature);
 
+uint16_t getHumidity(pHumidity self);
 void setHumidity(pHumidity self, uint16_t newMeasure);
 
 void task_Humidity(void* pvParameters);
